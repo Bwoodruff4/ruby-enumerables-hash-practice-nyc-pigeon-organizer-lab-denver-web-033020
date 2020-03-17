1c1dfd15b4ddbb9f5aa32b4fde16636b8125f625
Blake <blakewoodruff4@gmail.com>
2020-03-17 19:29:49 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_pigeon_hash = {}
+  
+  data.each{|prop,hash|
+    hash.each{|att,array|
+      array.each{|pigeon_name|
+        if !new_pigeon_hash.has_key?(pigeon_name)
+          new_pigeon_hash[pigeon_name]= {}
+        end
+        
+        if !new_pigeon_hash[pigeon_name].has_key?(prop)
+          new_pigeon_hash[pigeon_name][prop]= []
+        end
+        
+        if !new_pigeon_hash[pigeon_name][prop].include?(att)
+          new_pigeon_hash[pigeon_name][prop] << att.to_s
+        end
+      }
+    }
+  }
+  
+  new_pigeon_hash
 end
