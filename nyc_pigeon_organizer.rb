e17ae9a94964f14bb7baef3c90969c993f050dc8
Blake <blakewoodruff4@gmail.com>
2020-03-17 19:26:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,18 @@ def nyc_pigeon_organizer(data)
   new_pigeon_hash = {}
   
   data.each{|prop,hash|
-    ha
+    hash.each{|att,array|
+      array.each{|pigeon_name|
+        if !new_pigeon_hash.has_key?(pigeon_name)
+          new_pigeon_hash[pigeon_name]= {}
+        end
+        
+        if !new_pigeon_hash[pigeon_name].has_key?(prop)
+          new_pigeon_hash[pigeon_name][prop]= []
+        end
+        
+        if !new_pigeon_hash[pigeon_name][prop].include?(att)
+          new_pigeon_hash[pigeon_name][prop] << att.
   
   
   
